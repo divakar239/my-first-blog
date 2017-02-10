a7cfc66e466e8d416e112c827eb90fca5c7de61d
Divakar Kapil <divakar239@icloud.com>
2017-02-10 14:16:11 -0500

    Added view and template for detailed blog as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]													# We're assigning a view called post_list to the ^$ URL. the ^$ creates an empty string 
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post      			# the . infront of models means current directory; so we can use the name of the file without .py since theyare both in the same directory
 from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
 	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
-
-
-	return render(request,'blog/post_list.html',{'posts': posts}
+	return render(request,'blog/post_list.html',{'posts': posts}			# the {} at the end is the place holder which is used to add content to the template
+		)
+def post_detail(request,pk):
+	post=Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html',{'post': post}
 		)
